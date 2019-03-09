6fe13fb2b8966970e65279b86ed59b994c4e688d
rsh7 <rashi.747@gmail.com>
2019-03-09 18:52:55 +0530

    login files

---
+++ social/webserver/login.py
@@ -0,0 +1,35 @@
+from flask import Flask, redirect, url_for, render_template
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required, UserMixin
+
+class User(UserMixin):
+  def __init__(self,id):
+    self.id = id
+
+def init_login(app):
+	login_manager = LoginManager()
+	login_manager.init_app(app)
+	login_manager.login_view = 'bp.log_in'
+
+	@login_manager.user_loader
+	def load_user(user_id):
+	    return User(user_id)
+
+
+
+
+
+
+
+# @app.route('/login/')
+# def login():
+    
+#     return "you are logged in!"
+
+# @app.route('/logout/')
+# @login_required
+# def logout():
+#     logout_user()
+#     return "you are logged out!"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=1212,debug=True)

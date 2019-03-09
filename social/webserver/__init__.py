3f14f69c3cff72ce70526b2723ffd8ad99f03178
Param Singh <paramsingh258@gmail.com>
2019-03-09 16:31:10 +0530

    add api endpoints

--- social/webserver/__init__.py
+++ social/webserver/__init__.py
@@ -2,7 +2,11 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+
     from social.webserver.views import bp
+    from social.webserver.api import api
     app.register_blueprint(bp)
+    app.register_blueprint(api)
+
     return app
 

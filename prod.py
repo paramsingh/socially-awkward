2584296035f7bab5aaa4398c293cf1791b34c3e8
Param Singh <paramsingh258@gmail.com>
2019-03-10 12:03:42 +0530

    drop tables

--- prod.py
+++ prod.py
@@ -1,5 +1,7 @@
-from social.db import create_tables
+from social.db import create_tables, drop_tables
 from social.webserver import create_app
 
+if True:
+    drop_tables()
 create_tables()
 app = create_app()
--- social/db/__init__.py
+++ social/db/__init__.py
@@ -3,6 +3,15 @@ import sqlite3
 
 DATABASE_PATH = os.path.join('..', os.path.dirname(os.path.realpath(__file__)), 'socialize.sqlite3')
 
+def drop_tables():
+    with sqlite3.connect(DATABASE_PATH) as connection:
+        cursor = connection.cursor()
+        cursor.executemany("""
+            DROP TABLE IF EXISTS user;
+            DROP TABLE IF EXISTS post;
+            DROP TABLE IF EXISTS follow;
+        """)
+
 
 def create_tables():
     with sqlite3.connect(DATABASE_PATH) as connection:
